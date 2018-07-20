200f285f356404fc984df044a218ba0c07042659
Itang Samuel <31269938+shinnokmp@users.noreply.github.com>
2018-07-20 14:44:29 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# memorygame
+# Memory Game Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+# Memory Game Project
+
+## What is the Memory Game
+This is a simple game built to test a users memory. There's a deck of cards with different icons. 
+![snippet](img/snippet.PNG)
+
+## Challenge
+Match cards in less time with less moves.
+
+## Instructions
+* Click on a card
+* Keep revealing cards and working your memory to remember each unveiled card.
+* Match cards properly with less moves and in faster time
+
+
+## How I built the Memory Game
+* I manipulated the DOM with JS, altered part of the HTML and also styled the game
+* Created a deck of cards that shuffles when game is refreshed
+* Created a counter to count the number of moves made by player and timer to know the duration of a play
+* Addeded effects to cards when they match and are unmatched
+* Created a pop-up modal when player wins game
---
+++ js/app.js
@@ -0,0 +1,248 @@
+let mod = document.querySelector("mo");
+/*
+ * Create a list that holds all of your cards
+ */
+// cards array holds all cards
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+
+// deck of all cards in game
+const deck = document.getElementById("card-deck");
+
+// declaring move variable
+let moves = 0;
+let counter = document.querySelector(".moves");
+
+// declare variables for star icons
+const stars = document.querySelectorAll(".fa-star");
+
+// declaring variable of matchedCards
+let matchedCard = document.getElementsByClassName("match");
+
+ // stars list
+ let starsList = document.querySelectorAll(".stars li");
+
+ // close icon in modal
+ let closeicon = document.querySelector(".close");
+
+ // declare modal
+ let modal = document.getElementById("popover")
+
+ // array for opened cards
+var openedCards = [];
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+document.body.onload = letsPlay();
+function letsPlay(){
+    cards = shuffle(cards);
+    for (var i = 0; i< cards.length; i++) {
+      deck.innerHTML = "";
+       [].forEach.call(cards, function (item) {
+           deck.appendChild(item);
+       });
+    cards[i].classList.remove("show", "open", "match", "disabled");
+}
+    moves = 0;
+    counter.innerHTML = `${moves} move`;
+
+    for (var i=0; i<stars.length; i++){
+        stars[i].style.color = "#330505";
+        stars[i].style.visibility = "visible";
+    }
+
+    second = 0;
+    minute = 0;
+    hour = 0;
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+}
+
+var displayCard = function (){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+};
+
+function cardOpen(){
+    openedCards.push(this);
+    let len = openedCards.length;
+    if(len===2){
+        moveCounter();
+        if(openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+};
+
+function matched(){
+    openedCards[0].classList.add("match", "disabled");
+    openedCards[1].classList.add("match", "disabled");
+    openedCards[0].classList.remove("show", "open", "no-event");
+    openedCards[1].classList.remove("show", "open", "no-event");
+    openedCards = [];
+}
+
+
+function unmatched(){
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function(){
+        openedCards[0].classList.remove("show", "open", "no-event","unmatched");
+        openedCards[1].classList.remove("show", "open", "no-event","unmatched");
+        enable();
+        openedCards = [];
+    },1000);
+}
+
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}
+
+
+// @description count player's moves
+function moveCounter(){
+    moves++;
+    counter.innerHTML = `${moves} move`;
+    //start timer on first click
+    if(moves == 1){
+        second = 0;
+        minute = 0; 
+        hour = 0;
+        startTimer();
+    }
+    if (moves > 1){
+        counter.innerHTML = `${moves} moves`;
+    }
+    // setting rates based on moves
+    if (moves > 9 && moves < 12){
+        for( i= 0; i < 3; i++){
+            if(i < 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves >= 12 && moves < 15){
+        for( i= 0; i < 3; i++){
+            if(i == 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves >= 15 && moves < 20){
+        for( i= 0; i < 3; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves >= 20){
+        for( i= 0; i < 4; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
+
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs ";
+        second++;
+        if(second == 60){
+            minute++;
+            second=0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
+
+
+// @description congratulations when all cards match, show modal and moves, time and rating
+function congratulations(){
+    if (matchedCard.length == 16){
+        clearInterval(interval);
+        finalTime = timer.innerHTML;
+
+        // show congratulations modal
+        modal.classList.add("show");
+
+        // declare star rating variable
+        var starRating = document.querySelector(".stars").innerHTML;
+
+        //showing move, rating, time on modal
+        document.getElementById("finalMove").innerHTML = moves;
+        document.getElementById("starRating").innerHTML = starRating;
+        document.getElementById("totalTime").innerHTML = finalTime;
+
+        //closeicon on modal
+        closeModal();
+    };
+}
+
+
+// @description close icon on modal
+function closeModal(){
+    closeicon.addEventListener("click", function(e){
+        modal.classList.remove("show");
+        letsPlay();
+    });
+}
+
+
+// @desciption for user to play Again 
+function playAgain(){
+    modal.classList.remove("show");
+    letsPlay();
+}
+
+
+// loop to add event listeners to each card
+for (var i = 0; i < cards.length; i++){
+    card = cards[i];
+    card.addEventListener("click", displayCard);
+    card.addEventListener("click", cardOpen);
+    card.addEventListener("click", congratulations);
+};
